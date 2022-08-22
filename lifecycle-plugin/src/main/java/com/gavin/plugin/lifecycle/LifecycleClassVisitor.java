e0e71790b2bae03c0cdb4b21e51a0834991f8b96
liuhuiyu991026 <liuhuiyu991026@163.com>
2022-08-22 21:07:21 +0800

    AnkiDroid-debug-2.9alpha4-#4707

---
+++ lifecycle-plugin/src/main/java/com/gavin/plugin/lifecycle/LifecycleClassVisitor.java
@@ -0,0 +1,45 @@
+package com.gavin.plugin.lifecycle;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author gavin
+ * @date 2019/2/18
+ * lifecycle class visitor
+ */
+public class LifecycleClassVisitor extends ClassVisitor implements Opcodes {
+
+    private String mClassName;
+
+    public LifecycleClassVisitor(ClassVisitor cv) {
+        super(Opcodes.ASM5, cv);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+//        System.out.println("LifecycleClassVisitor : visit -----> started ：" + name);
+        this.mClassName = name;
+        super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+        return new LifecycleMethodVisitor(mv, mClassName, name);
+    }
+
+    @Override
+    public void visitEnd() {
+        //System.out.println("LifecycleClassVisitor : visit -----> end");
+        super.visitEnd();
+    }
+}
